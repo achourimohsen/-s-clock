66299551aa240b60d19c92b72d656678e3774542
achouri mohsen <achorimohsen@gmail.com>
2023-03-31 21:15:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const hour = document.getElementById("hour")
+const min = document.getElementById("minutes")
+const sec = document.getElementById("seconds")
+const ampm = document.getElementById("ampm")
+
+console.log(hour, min, sec)
+
+function updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h>12){
+        h = h -12
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hour.innerText = h
+    min.innerText = m
+    sec.innerText = s
+    ampm.innerText = ampm
+    
+    setTimeout(() => {
+        updateClock()
+    },1000)
+}
+
+updateClock()
+
